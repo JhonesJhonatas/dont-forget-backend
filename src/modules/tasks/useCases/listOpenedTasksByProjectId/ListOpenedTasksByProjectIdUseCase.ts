8f34ab6ff595931f3245c4ceec042a46e91bdb16
Jhones Jhonatas <jhones.jhonatas@outlook.com.br>
2023-10-26 12:53:25 -0300

    feat/getOpenedTasksByProjectId flow :art:

---
+++ src/modules/tasks/useCases/listOpenedTasksByProjectId/ListOpenedTasksByProjectIdUseCase.ts
@@ -0,0 +1,34 @@
+import { inject, injectable } from 'tsyringe'
+import { ITasksRepository } from '../../repositories/ITasksRepository'
+import { IProjectsRepository } from '../../../projects/repositories/IProjectsRepository'
+import { AppError } from '../../../../errors/AppError'
+
+@injectable()
+class ListOpenedTasksByProjectIdUseCase {
+  constructor(
+    @inject('ProjectsRepository')
+    private projectsRepository: IProjectsRepository,
+
+    @inject('TasksRepository')
+    private tasksRepository: ITasksRepository,
+  ) {}
+
+  async execute(userId: string, projectId: string) {
+    const listOfProjects = await this.projectsRepository.listProjects(userId)
+
+    const projectExists = listOfProjects.some(
+      (project) => project.id === projectId,
+    )
+
+    if (!projectExists) {
+      throw new AppError('O projeto informado não existe', 400)
+    }
+
+    const tasksList =
+      await this.tasksRepository.findOpenedTasksByProjectId(projectId)
+
+    return tasksList
+  }
+}
+
+export { ListOpenedTasksByProjectIdUseCase }
--- src/routes/tasks.routes.ts
+++ src/routes/tasks.routes.ts
@@ -2,12 +2,15 @@ import { Router } from 'express'
 import { ensureAuthenticated } from '../shared/middlewares/ensureAuthenticated'
 import { CreateTaskController } from '../modules/tasks/useCases/createTasks/CreateTaskController'
 import { EditTaskController } from '../modules/tasks/useCases/editTask/EditTaskController'
-import { ListAllOpenedTasksController } from '../modules/tasks/useCases/listOpenedTasks/ListOpenedTaskController'
+import { ListAllOpenedTasksController } from '../modules/tasks/useCases/listAllTasksByUserId/ListOpenedTaskController'
+import { ListOpenedTasksByProjectIdController } from '../modules/tasks/useCases/listOpenedTasksByProjectId/ListOpenedTasksByProjectIdController'
 
 const taskRoutes = Router()
 
 const createTaskController = new CreateTaskController()
-const listOpenedTasksController = new ListAllOpenedTasksController()
+const listOpenedTasksByUserIdController = new ListAllOpenedTasksController()
+const listOpenedTasksByProjectIdController =
+  new ListOpenedTasksByProjectIdController()
 const editTasController = new EditTaskController()
 
 taskRoutes.post(
@@ -17,9 +20,15 @@ taskRoutes.post(
 )
 
 taskRoutes.get(
-  '/list-all-opened-tasks',
+  '/list-all-opened-tasks-by-user-id',
   ensureAuthenticated,
-  listOpenedTasksController.handle,
+  listOpenedTasksByUserIdController.handle,
+)
+
+taskRoutes.get(
+  '/list-opened-tasks-by-project-id/:projectId',
+  ensureAuthenticated,
+  listOpenedTasksByProjectIdController.handle,
 )
 
 taskRoutes.put(
