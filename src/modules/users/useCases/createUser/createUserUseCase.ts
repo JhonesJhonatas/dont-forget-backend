2249e61752a479ab3b8627192491506f43523b46
Jhones Jhonatas <jhones.jhonatas@outlook.com.br>
2023-10-27 18:05:52 -0300

    feat/migrations :art:

--- src/modules/users/useCases/createUser/createUserUseCase.ts
+++ src/modules/users/useCases/createUser/createUserUseCase.ts
@@ -15,7 +15,7 @@ class CreateUserUseCase {
     const userAlreadyExists = await this.usersRepository.findByEmail(email)
 
     if (userAlreadyExists) {
-      throw new AppError('User already exists!')
+      throw new AppError('User already exists!', 400)
     }
 
     const passwordHash = await hash(password, 8)
