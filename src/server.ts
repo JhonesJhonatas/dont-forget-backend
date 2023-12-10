9911d27de3ed74a20c6af3a2abcdbdffbda8b42c
Jhones Jhonatas <jhones.jhonatas@outlook.com.br>
2023-12-10 02:45:08 -0300

    fix/changePortToDeploy

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ import { routes } from './routes'
 import { AppError } from './errors/AppError'
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 80
 
 app.use(express.json())
 
