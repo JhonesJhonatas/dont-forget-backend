6b19c984abd2689f169a4e69f3e0a85f7bd66043
Jhones Jhonatas <jhones.jhonatas@outlook.com.br>
2023-12-21 21:51:16 -0300

    feat/excludeTasksRelationsWithProject :art:

--- src/server.ts
+++ src/server.ts
@@ -16,18 +16,20 @@ app.use(cors())
 
 app.use(routes)
 
-app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
-  if (err instanceof AppError) {
-    return response.status(err.statusCode).json({
-      message: err.message,
-    })
-  }
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        message: err.message,
+      })
+    }
 
-  return response.status(500).json({
-    status: 'error',
-    message: `Internal server error - ${err.message}`,
-  })
-})
+    return response.status(500).json({
+      status: 'error',
+      message: `Internal server error - ${err.message}`,
+    })
+  },
+)
 
 app.listen(port, () => {
   console.log(`Server running on port: ${port}. 🚀🚀`)
