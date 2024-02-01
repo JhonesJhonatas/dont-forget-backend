ed942d741bf27d2fdfcf593238ecf81dc8f909b3
Jhones Jhonatas <jhones.jhonatas@outlook.com.br>
2024-02-01 20:47:53 -0300

    fix/insertNewDomainsInCors :bug:

--- src/server.ts
+++ src/server.ts
@@ -17,7 +17,12 @@ mongoClient.connect()
 app.use(express.json())
 
 const corsOptions = {
-  origin: ['https://dont-forget-web.vercel.app', 'http://127.0.0.1:5173'],
+  origin: [
+    'https://dont-forget-web.vercel.app',
+    'http://127.0.0.1:5173',
+    'www.dontforget.com.br',
+    'dontforget.com.br',
+  ],
   optionsSuccessStatus: 200,
 }
 
