b658bbce9a0056b5dcba4dc536ad067b10375c07
Jhones Jhonatas <jhones.jhonatas@outlook.com.br>
2024-02-21 22:13:50 -0300

    feat: create progress route

--- src/routes/projects.routes.ts
+++ src/routes/projects.routes.ts
@@ -5,6 +5,7 @@ import { ListProjectsController } from '../modules/projects/useCases/listProject
 import { EditProjectController } from '../modules/projects/useCases/editProject/EditProjectController'
 import { DeleteProjectController } from '../modules/projects/useCases/deleteProject/DeleteProjectController'
 import { GetProjectByIdController } from '../modules/projects/useCases/getProjectById/GetProjectByIdController'
+import { GetProjectProgressController } from '../modules/projects/useCases/getProjectProgress/GetProjectProgressController'
 
 const projectRoutes = Router()
 
@@ -13,6 +14,7 @@ const listProjectsController = new ListProjectsController()
 const getProjectByIdController = new GetProjectByIdController()
 const editProjectController = new EditProjectController()
 const deleteProjectController = new DeleteProjectController()
+const getProjectProgressController = new GetProjectProgressController()
 
 projectRoutes.post(
   '/create-project',
@@ -30,6 +32,12 @@ projectRoutes.get(
   '/get-project-by-id/:projectId',
   ensureAuthenticated,
   getProjectByIdController.handle,
+)
+
+projectRoutes.get(
+  '/get-project-progress/:projectId',
+  ensureAuthenticated,
+  getProjectProgressController.handle,
 )
 
 projectRoutes.put(
